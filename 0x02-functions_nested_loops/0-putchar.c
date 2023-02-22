126ca8280aa90c139d5e5e1dead874019cee3d06
Amirastemp <mansouriamira516@gmail.com>
2023-02-22 07:41:37 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main-Print_putchar
+*
+* Return:Always0
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+return (0);
+}
